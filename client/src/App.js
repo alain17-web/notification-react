9ef574f99c9b17495a7fa596d614d53d0babb494
alain17-web <web2020.alain@gmail.com>
2022-09-20 17:01:52 +0200

    visuel

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import { useState } from 'react'
 import './App.css';
+import Card from './components/card/Card';
+import Navbar from './components/navbar/Navbar';
+import { posts } from './data'
 
 function App() {
+
+  const [username, setUsername] = useState("")
+  const [user, setUser] = useState("")
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {user ? (
+        <>
+        <Navbar/>
+        {posts.map((post) => {
+          return <Card key={post.id} post={post}/>
+        })}
+        
+        <span className='username'>{user}</span>
+        </>
+      ) : (
+      <div className="login">
+        <input type="text" placeholder="username" onChange={(e)=> setUsername(e.target.value)}/>
+        <button onClick={() => setUser(username)}>Login</button>
+      </div>
+      )}
     </div>
   );
 }
---
+++ client/src/components/card/Card.jsx
@@ -0,0 +1,30 @@
+import './card.css'
+import Heart from '../../img/heart.svg'
+import HeartFilled from '../../img/heart.svg'
+import Comment from '../../img/comment.svg'
+import Share from '../../img/share.svg'
+import Info from '../../img/info.svg'
+import { useState } from 'react'
+
+const Card = ({post}) => {
+
+  const [liked,setLiked] = useState(false)
+
+  return (
+    <div className='card'>
+      <div className="info">
+        <img src={post.userImg} alt="" className='userImg'/>
+        <span>{post.fullname}</span>
+      </div>
+      <img src={post.postImg} alt="" className='postImg'/>
+      <div className="interaction">
+        <img src={Heart} alt="" className="cardIcon" />
+        <img src={Comment} alt="" className="cardIcon" />
+        <img src={Share} alt="" className="cardIcon" />
+        <img src={Info} alt="" className="cardIcon infoIcon" />
+      </div>
+    </div>
+  )
+}
+
+export default Card
